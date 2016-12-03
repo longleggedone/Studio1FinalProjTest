b5556e6483835f2f6e3c421293d26ceccdfcb6da
longleggedone <longlegsclassic@gmail.com>
2016-12-03 15:45:15 -0500

    moved files to new project folder

---
+++ Assets/Scripts/PoemTrigger.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PoemTrigger : MonoBehaviour {
+
+	public GameObject managerObject;
+	public GameObject canvas;
+	private FadeCanvas fader;
+	public UIManager textManager;
+	public string newText;
+
+	void Awake(){
+		managerObject = GameObject.FindGameObjectWithTag("UIManager");
+		canvas = GameObject.FindGameObjectWithTag("Canvas");
+		fader = canvas.GetComponent<FadeCanvas>();
+		textManager = managerObject.GetComponent<UIManager>();
+		textManager.displayText = newText;
+		//Debug.Log("Enter");
+		fader.FadeIn();
+
+	}
+
+
+}
---
+++ Assets/Scripts/SceneLoadOnTrigger.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class SceneLoadOnTrigger: MonoBehaviour {
+
+	//public string currentScene;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void OnTriggerEnter (Collider other) {
+
+		if (other.CompareTag("Player"))
+			SceneManager.LoadScene ("PoemScene");
+	
+	}
+}
